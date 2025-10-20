759065582642e13227d38281ea9f31729788fc5c
UNbreak4ble <diego.gamere1921@outlook.com>
2025-10-20 00:03:04 -0400

    docker firewall

--- scripts/03-setup-container-services.sh
+++ scripts/03-setup-container-services.sh
@@ -4,6 +4,17 @@ set -e
 
 fixed_dir=$(readlink -f ${0%/*})
 
+echo "configurando firewall do docker"
+
+iptables -t nat -F
+iptables -t nat -X DOCKER
+iptables -t nat -N DOCKER
+iptables -t nat -A PREROUTING -m addrtype --dst-type LOCAL -j DOCKER
+iptables -t nat -A OUTPUT ! -d 127.0.0.0/8 -m addrtype --dst-type LOCAL -j DOCKER
+iptables -t nat -A DOCKER -i docker0 -j RETURN
+
+systemctl restart docker
+
 echo "preparando containers nescessarios"
 
 for script in $(ls $fixed_dir/container-services); do
---
+++ scripts/97-chown.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+username=$1
+target_dirs={"/home/$username/server.git","/srv/server"}
+
+echo "transferindo permissões para o usuario $username"
+
+for target_dir in $target_dirs; do;
+    chown -R $username:$username $target_dir;
+done;
+
+echo "permissões transferidas"
