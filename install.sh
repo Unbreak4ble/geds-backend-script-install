e647f3f06afa51a104231faf0d9a106935e29e35
UNbreak4ble <diego.gamere1921@outlook.com>
2025-10-19 23:17:02 -0400

    install.sh and readme

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+set -e
+
+target_dir=/var/tmp/server_install_files
+source_files_url='https://github.com/Unbreak4ble/geds-backend-script-install/archive/refs/tags/v1.0.0.zip'
+
+mkdir -p $target_dir
+
+cd $target_dir
+
+echo "Fazendo download dos arquivos"
+
+wget -O source.zip $source_files_url
+
+echo "Descomprimindo arquivos"
+
+unzip source.zip
+
+rm source.zip
+
+echo "executando"
+
+./root-wrapper.sh
