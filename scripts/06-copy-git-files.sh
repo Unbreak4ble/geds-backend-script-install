9efcb2e574bcc12fb25efdb04d8abd816fb41f4f
UNbreak4ble <diego.gamere1921@outlook.com>
2025-10-19 23:02:49 -0400

    forgot git hooks

---
+++ scripts/06-copy-git-files.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+set -e
+
+fixed_dir=$(readlink -f ${0%/*})
+username=$1
+
+echo "copiando arquivos git hooks"
+
+cp -rf $fixed_dir/../assets/git_hooks/* /home/$username/server.git/hooks/
+
+echo "arquivos copiados para /etc/server"
