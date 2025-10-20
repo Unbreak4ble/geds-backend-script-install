cbf16be8cd83c128bb24ccb0f4c9e4d9eb79b656
UNbreak4ble <diego.gamere1921@outlook.com>
2025-10-20 07:29:47 -0400

    chown.sh

--- uninstall.sh
+++ uninstall.sh
@@ -8,5 +8,8 @@ sudo rm -rf /srv/server
 sudo rm -rf /etc/server
 sudo docker kill postgres
 sudo docker rm postgres
+sudo systemctl stop server
+sudo systemctl disable server
+sudo rm /etc/systemd/system/server.service
 
 echo "servidor desinstalado"
