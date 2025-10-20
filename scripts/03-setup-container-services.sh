75afa362110b8070ddea573c99bf2760a1b5702f
UNbreak4ble <diego.gamere1921@outlook.com>
2025-10-19 23:46:22 -0400

    setup-container-service.sh

--- scripts/03-setup-container-services.sh
+++ scripts/03-setup-container-services.sh
@@ -7,7 +7,7 @@ fixed_dir=$(readlink -f ${0%/*})
 echo "preparando containers nescessarios"
 
 for script in $(ls $fixed_dir/container-services); do
-	bash $fixed_dir/$script
+	bash $fixed_dir/container-services/$script
 	ret_status=$?
 	if ! [ "$ret_status" = "0" ]; then
 		exit 1;
